99a89dc34562c56e453fc725690c5b084a5dbdcd
Dileep <dileepkumawat525@gmail.com>
2026-04-20 17:05:48 +0530

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Added package.json with dependencies and scripts for development
    - Created App component with routing setup
    - Implemented basic routing with a Hello World page
    - Set up Redux store with authentication slice
    - Developed authentication hooks and API service for user registration
    - Configured Vite for React project
    - Added Tailwind CSS for styling

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -2,10 +2,26 @@ import userModel from '../models/user.model.js';
 import jwt from 'jsonwebtoken';
 import { config } from '../config/config.js';
 
-async function sendTokenResponse(user, res) {
+async function sendTokenResponse(user, res, message) {
     const token = jwt.sign({
         id: user._id
-    }, config.JWT_SECRET);
+    }, config.JWT_SECRET, {
+        expiresIn: "7d"
+    });
+
+    res.cookie("token", token);
+
+    res.status(200).json({
+        message,
+        success: true,
+        user: {
+            id: user._id,
+            email: user.email,
+            contact: user.contact,
+            fullname: user.fullname,
+            role: user.role
+        }
+    });
 }
 
 export const register = async (req, res) => {
@@ -27,10 +43,11 @@ export const register = async (req, res) => {
             email,
             contact,
             password,
-            fullname
+            fullname,
+            role: isSeller ? "seller" : "buyer"
         });
 
-        
+        await sendTokenResponse(user, res, "User registered successfully");
     } catch (error) {
         console.log(error)
         return res.status(500).json({ message: "Server error" });
---
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -0,0 +1,14 @@
+import { setUser, setLoading, setError } from '../slice/auth.slice.js';
+import { register } from '../services/auth.api.js';
+import { useDispatch } from 'react-redux';
+
+export const useAuth = () => {
+    const dispatch = useDispatch();
+
+    async function handleRegister({ email, contact, password, fullname, isSeller = false }) {
+        const data = await register({ email, contact, password, fullname, isSeller })
+        dispatch(setUser(data.user))
+    }
+
+    return { handleRegister }
+}
