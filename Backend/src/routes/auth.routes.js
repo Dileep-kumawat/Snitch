d8c3e29643eeabb15b91ec1d94f09dc80d99af55
Dileep <dileepkumawat525@gmail.com>
2026-04-20 16:17:59 +0530

    Backend Initiated with register route

---
+++ Backend/src/routes/auth.routes.js
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import { validateRegisterUser } from '../validators/auth.validator.js';
+
+const router = Router();
+
+router.post('/register', validateRegisterUser);
+
+export default router;
